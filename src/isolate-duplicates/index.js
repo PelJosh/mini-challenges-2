9e81ebbf9afd8d567d810e98ea2969343cae99b7
PelJosh <joshua.oyedola@decagon.dev>
2022-11-29 11:05:50 +0100

    Solution to mini challenge 2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,13 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  let chars = ("aabbabccddcdc");
+  let duplicateChars = chars.filter((c, index) => {
+    isolateDuplicates(chars.filter);
+  })
+  isolateDuplicates
+
+}
+
+var myName = "JoshuaOyeJoshua";
+
 
 module.exports = isolateDuplicates;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,11 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  let chars = ['A','B','B','C','A','C','A','B'];
+  let duplicateChars = [];
+  chars.forEach((c) => {
+    if(!duplicateChars.includes(c)) {
+      duplicateChars.push(c);
+    }
+  })
+}
 
 module.exports = removeDuplicates;
